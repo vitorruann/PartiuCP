e5fe5bc2ce77c03db0a4c352427674787a135667
unknown <vitorruann@gmail.com>
2019-11-25 21:15:13 -0300

    adicionado o material ui no frontc

--- frontend/frontc.bat
+++ frontend/frontc.bat
@@ -8,6 +8,6 @@ echo ***************************************************************************
 echo ESPERE A JANELA ANTERIOR FECHAR PARA CONTINUAR!!!!
 echo ***************************************************************************
 
-yarn add axios cors react-router-dom
-
-exit
+yarn add axios cors react-router-dom @material-ui/core
+pause
+yarn add @material-ui/icon
